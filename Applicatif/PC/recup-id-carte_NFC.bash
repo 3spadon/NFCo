581f28182c2e5951021e486a2d323eafb388cc98
Unknown <zevoboss@gmail.com>
2017-10-22 21:28:10 +0200

    recup-id_v1.2

--- Applicatif/PC/recup-id-carte_NFC.bash
+++ Applicatif/PC/recup-id-carte_NFC.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
+#author: SC
+# affiche et enregistre uniquement l'id, pas de persistence des données ( écrasement des données précédentes quand on relance le script )
+echo "Passez le badge devant le lecteur"
 explorenfc-basic > temp.txt
+echo "----------------"
 more +6 temp.txt > tid
 cut -c17-32 tid > id_carte_nfc.txt
+echo "ID de la carte lue: "
 cat id_carte_nfc.txt
 rm -f tid temp.txt
